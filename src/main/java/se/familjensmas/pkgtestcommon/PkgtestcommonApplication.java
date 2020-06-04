d221236161caf9a5ebb1ac355fc4bc63d36a5962
Jörgen <jorgen.smas@trustly.com>
2020-06-04 12:58:26 +0200

    Added files

---
+++ src/main/java/se/familjensmas/pkgtestcommon/PkgtestcommonApplication.java
@@ -0,0 +1,13 @@
+package se.familjensmas.pkgtestcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkgtestcommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkgtestcommonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/familjensmas/pkgtestcommon/PkgtestcommonApplicationTests.java
@@ -0,0 +1,13 @@
+package se.familjensmas.pkgtestcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkgtestcommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
